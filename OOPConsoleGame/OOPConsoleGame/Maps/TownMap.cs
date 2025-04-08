88aeab078b38ed167acb3b94ed30ef50e795ca6d
NormalSeed <dlrjsghk11@naver.com>
2025-04-08 14:13:16 +0900

    [Feat] 맵 인터페이스 구현
    맵 기능을 인터페이스로 구현해서 따로 관리할 수 있게 함

---
+++ OOPConsoleGame/OOPConsoleGame/Maps/TownMap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleGame.Maps
+{
+    public class TownMap : IMap
+    {
+        private string[] mapData;
+        private bool[,] map;
+        public void CreateMap()
+        {
+            mapData = new string[]
+            {
+                "###############",
+                "#        ▲    #",
+                "##            #",
+                "#             #",
+                "#           ###",
+                "#             #",
+                "#             #",
+                "#   ▲▲▲       #",
+                "#   ▲▲        #",
+                "###############"
+            };
+            map = new bool[10, 15];
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 15; x++)
+                {
+                    map[y, x] = mapData[y][x] == ' ' ? true : false;
+                }
+            }
+        }
+        public void PrintMap()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] == true)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write(mapData[y][x]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- OOPConsoleGame/OOPConsoleGame/Scenes/TownScene.cs
+++ OOPConsoleGame/OOPConsoleGame/Scenes/TownScene.cs
@@ -3,41 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOPConsoleGame.Maps;
 
 namespace OOPConsoleGame.Scenes
 {
     public class TownScene : BaseScene
     {
-        private string[] mapData;
-        private bool[,] map;
+        private TownMap townMap;
 
-        public TownScene()
-        {
-            mapData = new string[]
-            {
-                "###############",
-                "#        ▲    #",
-                "##            #",
-                "#             #",
-                "#           ###",
-                "#             #",
-                "#             #",
-                "#   ▲▲▲       #",
-                "#   ▲▲        #",
-                "###############"
-            };
-            map = new bool[10, 15];
-            for (int y = 0; y < 10; y++)
-            {
-                for (int x = 0; x < 15; x++)
-                {
-                    map[y, x] = mapData[y][x] == ' ' ? true : false;
-                }
-            }
-        }
         public override void Render()
         {
-            PrintMap();
+            townMap = new TownMap();
+            townMap.CreateMap();
+            townMap.PrintMap();
             Console.WriteLine("당신은 꿈에서 깨어난 뒤, 손에 새겨진 문양의 의미를 알기 위해 움직입니다.");
             Console.WriteLine("눈앞에 펼쳐진 마을 광장은 여느때처럼 한산합니다.");
             Console.WriteLine();
@@ -50,26 +28,7 @@ namespace OOPConsoleGame.Scenes
 
         public override void Result()
         {
-            
-        }
-        private void PrintMap()
-        {
-            Console.SetCursorPosition(0, 0);
-            for (int y = 0; y < map.GetLength(0); y++)
-            {
-                for (int x = 0; x < map.GetLength(1); x++)
-                {
-                    if (map[y, x] == true)
-                    {
-                        Console.Write(' ');
-                    }
-                    else
-                    {
-                        Console.Write(mapData[y][x]);
-                    }
-                }
-                Console.WriteLine();
-            }
+
         }
     }
 }
