3bbcd446a4fce33313f91e3309b3d6a28556b87b
NormalSeed <dlrjsghk11@naver.com>
2025-04-08 18:04:51 +0900

    [Feat] Wait 유틸 작성
    다음으로 넘어가기 전 아무 키나 누르는 입력을 받는 유틸 구현

---
+++ OOPConsoleGame/OOPConsoleGame/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleGame
+{
+    public static class Util
+    {
+        // 다음으로 진행 전 대기 (계속하려면 아무 키나 누르세요.)
+        public static void Wait()
+        {
+            Console.WriteLine();
+            Console.WriteLine("계속 진행하려면 아무 키나 누르세요.");
+            Console.ReadKey();
+        }
+    }
+}
