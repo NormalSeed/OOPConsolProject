abc7993da0a15fdff2680a84cf7c2b24901dc9d5
NormalSeed <dlrjsghk11@naver.com>
2025-04-08 11:19:40 +0900

    [Feat] 씬 동작 프레임워크 제작
    게임 클래스가 여러 씬들을 가질 수 있도록 구현하여 추후 씬들을 추가해가며 동작할 수 있게 함

--- OOPConsoleGame/OOPConsoleGame/Game.cs
+++ OOPConsoleGame/OOPConsoleGame/Game.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using OOPConsoleGame.Scenes;
 
 namespace OOPConsoleGame
 {
     internal class Game
     {
+        private static bool gameOver;
+
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
         public static void Start()
         {
+            gameOver = false;
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
 
+            curScene = sceneDic["Title"];
         }
         public static void Run()
         {
+            Start();
 
+            while (gameOver == false)
+            {
+                Console.Clear();
+                curScene.Render();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result(); 
+            }
         }
         public static void End()
         {
---
+++ OOPConsoleGame/OOPConsoleGame/Scenes/TitleScene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleGame.Scenes
+{
+    public class TitleScene : BaseScene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine("*     제목 미정     *");
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+            Console.WriteLine("계속하려면 아무 키나 눌러주세요.");
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Result()
+        {
+            
+        }
+    }
+}
