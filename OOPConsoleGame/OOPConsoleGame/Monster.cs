5803fe03fe8d7de288367bacb8681bd0c11defcf
NormalSeed <dlrjsghk11@naver.com>
2025-04-10 03:30:53 +0900

    [Feat] 몬스터 팩토리 구현
    몬스터 클래스와 몬스터를 만들어내는 몬스터 팩토리 클래스 구현

---
+++ OOPConsoleGame/OOPConsoleGame/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleGame
+{
+    public class Monster
+    {
+        public string name;
+        public int level;
+        public int maxHp;
+        public int hp;
+        public int atk;
+        public int def;
+        public int spd;
+
+        public Monster(string name, int level, int maxHp, int hp, int atk, int def, int spd)
+        {
+            this.name = name;
+            this.level = level;
+            this.maxHp = maxHp;
+            this.hp = hp;
+            this.atk = atk;
+            this.def = def;
+            this.spd = spd;
+        }
+    }
+}
--- OOPConsoleGame/OOPConsoleGame/MonsterFactory.cs
+++ OOPConsoleGame/OOPConsoleGame/MonsterFactory.cs
@@ -8,5 +8,14 @@ namespace OOPConsoleGame
 {
     public class MonsterFactory
     {
+        public Monster Create(string name)
+        {
+            switch (name)
+            {
+                case "고블린":
+                    return new Monster("고블린", 1, 7, 7, 1, 1, 2);
+            }
+            return null;
+        }
     }
 }
